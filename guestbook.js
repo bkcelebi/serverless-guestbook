1dd23c78a1e9479dac2c24bc12ed31a5bd88f9a6
Burak <98746081+bkcelebi@users.noreply.github.com>
2022-04-03 16:12:11 +0100

    API URL updated

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://7af6744a.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
